11560aea1ce87f304094c767ee146d66e5eac28e
Md Siwom Chowdhury <77885884+mdSiwom@users.noreply.github.com>
2023-11-13 03:32:59 +0600

    Add files via upload

---
+++ src/IDandPasswords.java
@@ -0,0 +1,21 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class IDandPasswords {
+
+	User[] logininfo = new User[4];
+
+	IDandPasswords() {
+		this.logininfo[0] = new User("Anika", "123");
+		this.logininfo[1] = new User("Ebadi", "123");
+		this.logininfo[2] = new User("Rose", "123");
+		this.logininfo[3] = new User("Siwom", "123");
+	}
+
+	public User[] getLoginInfo() {
+		return logininfo;
+	}
+
+}
